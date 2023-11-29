03127ace88a5d740dfea7632f737d7fd51b88466
krishna-200 <20mh1a4242@acoe.edu.in>
2023-11-29 14:00:41 +0530

    url updated

--- src/url.js
+++ src/url.js
@@ -1,4 +1,6 @@
-export const URL = "http://localhost:3000";
-export const IF = "http://localhost:3000/images/";
+//export const URL = "http://localhost:3000";
+export const URL = "https://blog-market-xq1k.onrender.com";
+export const IF = "https://blog-market-xq1k.onrender.com/images/";
+//export const IF = "http://localhost:3000/images/";
 
 export default URL;
